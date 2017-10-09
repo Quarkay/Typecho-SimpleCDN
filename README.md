7fd06dbc84da3e1843cca0a75428719b2af00888
Huspy <mierhuo@gmail.com>
2017-10-10 00:12:03 +0800

    支持正则替换、支持多条替换规则;改变目录结构

--- README.md
+++ README.md
@@ -1,9 +1,37 @@
-#### Typecho SimpleCDN 插件
+Typecho-SimpleCDN
+----
 
-用于将页面内容替换，可用于镜像缓存类CDN。
+### 简介
 
-安装插件之后设置如下图示例，则可以将上传的静态图片、附件在渲染的时候改到镜像地址：
+本插件用于将页面内部分内容（在模板渲染前）按照设置的规则进行替换。可用于镜像缓存类CDN，同时也是此类需求最常见的场景，故取名为SimpleCDN。
+
+注意此插件只是将内容在渲染前动态更改，这样之后镜像CDN如果不用了或者出问题，关闭插件即可，就不用担心内容怎么处理了。
+
+### 安装
+
+1. 首先将本项目克隆到本地：
+
+    ```bash
+    git clone git@github.com:mierhuo/Typecho-SimpleCDN.git
+    ```
+
+2. 将子文件夹 SimpleCDN 复制到 Typecho插件目录
+
+    ```bash
+    cp -r Typecho-SimpleCDN/SimpleCDN /path...to...your...typecho/usr/plugins/
+    ```
+3. 在Typecho后台点击启用并进行相关设置即可
+
+### 设置说明
+
+#### 一般用法
+
+启用插件之后点击设置，若如下图所示进行设置(规则分割符留空、镜像CDN地址需根据情况自行设置)，则可以将上传的静态图片、附件在渲染的时候改到相应的镜像CDN地址：
 
 <img src="https://github.com/mierhuo/SimpleCDN/blob/master/example.png?raw=true" alt="example.jpg">
 
-这样做还是很有意义的，因为只是在渲染的时候动态更改，这样之后镜像CDN如果不用了或者出问题，关闭插件即可，就不用担心内容怎么处理了。
+#### 正则替换
+
+
+
+#### 多条替换
---
+++ SimpleCDN/Plugin.php
@@ -0,0 +1,119 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+/**
+ * 简易替换插件，可用作镜像类CDN地址转换，方便以后整站迁移。
+ *
+ * @package SimpleCDN
+ * @author Huspy
+ * @version 1.0.0
+ * @link https://www.mierhuo.com
+ */
+class SimpleCDN_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     *
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate(){
+        Typecho_Plugin::factory("Widget_Archive")->beforeRender = array('SimpleCDN_Plugin', 'replace_content');
+    }
+
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     *
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate(){
+    }
+
+    /**
+     * 获取插件配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form){
+        // 设置替换分割符和替换规则
+        $sep_flag = new Typecho_Widget_Helper_Form_Element_Text('sep_flag', NULL, '-_-', _t('规则分割符'), _t('规则分割符默认为 -_- ，可根据需要进行设置。'));
+        $to_replace = new Typecho_Widget_Helper_Form_Element_Text('to_replace', NULL, '', _t('替换前地址'));
+        $replace_to = new Typecho_Widget_Helper_Form_Element_Text('replace_to', NULL, '', _t('替换后地址'));
+        $form->addInput($sep_flag);
+        $form->addInput($to_replace);
+        $form->addInput($replace_to);
+    }
+
+    /**
+     * 个人用户的配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+
+    /**
+     * 插件实现方法
+     *
+     * @access public
+     * @param Widget_Archive $archive
+     * @return void
+     */
+    public static function replace_content(Widget_Archive $archive)
+    {
+        // 先得到函数参数避免重复运算
+        list($to_replace, $replace_to) = self::prepare_replace_args();
+
+        // 依次对队列中文章的指定内容进行替换
+        foreach($archive->stack as $index=>$con){
+            // 替换文章标题
+            if(array_key_exists('text', $con)){
+                $archive->stack[$index]['text'] = preg_replace($to_replace, $replace_to, $con['text']);
+            }
+            // 替换文章内容
+            if(array_key_exists('title', $con)){
+                $archive->stack[$index]['title'] = preg_replace($to_replace, $replace_to, $con['title']);
+            }
+        }
+
+        // 对 Widget_Archive->row 中对应信息进行更新
+        $archive->text = preg_replace($to_replace, $replace_to, $archive->text);
+        $archive->title = preg_replace($to_replace, $replace_to, $archive->title);
+    }
+
+    /**
+     * 准备替换规则参数
+     *
+     */
+    private static function prepare_replace_args()
+    {
+        // 获取规则分割符
+        $sep_flag = Typecho_Widget::widget('Widget_Options')->plugin('SimpleCDN')->sep_flag;
+
+        // 分割得到规则并进行预处理
+        $to_replace = explode($sep_flag, Typecho_Widget::widget('Widget_Options')->plugin('SimpleCDN')->to_replace);
+        $to_replace = array_map(self::sep_callback(), $to_replace);
+        $replace_to = explode($sep_flag, Typecho_Widget::widget('Widget_Options')->plugin('SimpleCDN')->replace_to);
+        return array($to_replace, $replace_to);
+    }
+
+    /**
+     * 替换规则预处理
+     *
+     * @return Callback Function
+     */
+    private static function sep_callback()
+    {
+        return function($item){
+            // 若本身就是Pattern参数格式，则无需处理
+            return preg_match('/^\/.*\/[a-z]*$/i', $item) ? $item : '/'.$item.'/';
+        };
+    }
+
+}
